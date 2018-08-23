43335fd71b1243193ae9e589821846bd3617746e
Biel Simon Rojas <bielsimon2@gmail.com>
2018-08-23 12:33:01 +0200

    Added nullcheck for navigation context at NavigationSpy.tsx

--- src/NavigationContext/NavigationSpy.tsx
+++ src/NavigationContext/NavigationSpy.tsx
@@ -2,6 +2,7 @@ import React, {Component, ReactNode} from 'react';
 import _ from "lodash";
 import PageContextSpy from "../PageContext/PageContextSpy";
 import {NavigationConsumer, ProvidedNavigationContext} from "./NavigationContext";
+import {ProvidedPageContext} from "../PageContext/PageContext";
 
 
 export interface NavigationSpyProps {
@@ -13,9 +14,14 @@ export interface NavigationSpyProps {
 class NavigationSpy extends Component<NavigationSpyProps> {
     render() {
         return <PageContextSpy>
-            {(pageContext) => {
+            {(pageContext: ProvidedPageContext | null) => {
+                // Can be used inside and outside a page
                 return <NavigationConsumer>
                     {(context: ProvidedNavigationContext) => {
+                        if(context == null){
+                            console.error('NavigationSpy used outside of a NavigationContext')
+                            return null;
+                        }
                         return this.props.children({
                             ...context,
                             navigate: context.navigateFromPage({from: (pageContext || {} as any).location || null})
