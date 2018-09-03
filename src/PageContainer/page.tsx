e641bf9192b805453fcd89f5fd1c56f118fe1d16
Biel Simon Rojas <bielsimon2@gmail.com>
2018-09-03 14:57:01 +0200

    Preformance imporvement at page.tsx Deep equality check on args.

--- src/PageContainer/page.tsx
+++ src/PageContainer/page.tsx
@@ -2,10 +2,24 @@ import * as React from "react";
 import PageContainer, {PageContainerProps} from "./PageContainer";
 import {SFC} from "react";
 import {ProvidedPageContext} from "../PageContext/PageContext";
+import {StackFrameLocation} from "../NavigationContext/NavigationContext";
+import * as _ from "lodash";
 
 
-export const page = (component): SFC<PageContainerProps> => (props) => {
-    return <PageContainer {...props} component={component}/>
+export const page = (component) => class extends React.Component<{location: StackFrameLocation, args: Object}, any> {
+    componentDidMount(){
+        // console.log(`Page ${component.displayName} mounted with args`, this.props.args);
+    }
+    componentWillUnmount(){
+        // console.log(`Page ${component.displayName} unmounted`);
+    }
+    shouldComponentUpdate(nextProps, nextState){
+        if(_.isEqual(this.props.args, nextProps.args)) return true
+        return false
+    }
+    render() {
+        return <PageContainer {...this.props} component={component}/>
+    }
 }
 
 
